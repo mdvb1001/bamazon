87517121e9198d001286d95f8784a29d7131e26b
maxvanbel <maxvanbel@gmail.com>
2017-01-14 09:54:49 -0600

    add comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,12 @@
+// request inquirer 
 var inquirer = require('inquirer');
+// request mysql package 
 var mysql = require('mysql');
+// link to key.js file for password of mysql
 var key = require('./key.js');
+// empty array set up for ID validation 
 var idArray = [];
+// connection settings
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -9,6 +14,7 @@ var connection = mysql.createConnection({
     password: key.mysqlKey.password,
     database: 'Bamazon'
 });
+// establish connection to server 
 connection.connect(function (err) {
     if (err) throw err;
     reset().catch(function (err) {
@@ -16,25 +22,30 @@ connection.connect(function (err) {
     });
 });
 
+// function that starts and reset the app (main menu)
 function reset() {
+    // prompt menu 
     return inquirer.prompt([{
         type: 'list',
         message: 'What would you like to do?',
         choices: ['Shop', 'Exit App'],
         name: 'menu'
     }]).then(function (answer) {
+        // display items before shopping 
         if (answer.menu === 'Shop') {
             showItems().then(function () {
                 return purchasePrompt();
             });
+        // this is for exiting the app 
         } else if (answer.menu === 'Exit App') {
             return exitApp();
         }
     });
 }
-
+// function for displaying for items
 function showItems() {
     return new Promise(function (success, failure) {
+        // selects everything in the database 'products'
         connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw (err);
             success(res);
@@ -46,12 +57,14 @@ function showItems() {
     });
 }
 
+// function for shopping, prompts ID and Quantity input
 function purchasePrompt() {
     return inquirer.prompt([{
         name: 'idOfItem',
         type: 'input',
         message: 'Enter ID of item to buy',
         validate: function (input) {
+            //validation: must be a number, an ID smaller than length of idArray and can't be empty string 
             if ((isNaN(input) !== true) && (input <= idArray.length) && (input !== '')) {
                 return true;
             } else {
@@ -64,6 +77,7 @@ function purchasePrompt() {
         type: 'input',
         message: 'Enter quantity to buy',
         validate: function (input) {
+            // Cannot not be a number 
             if (isNaN(input) !== true) {
                 return true;
             } else {
@@ -72,31 +86,40 @@ function purchasePrompt() {
             }
         }
     }]).then(function (answer) {
+        // new Promise to select to THEN select all the ids of table 
         return new Promise(function (success, failure) {
             connection.query("SELECT * FROM products WHERE item_id=?", answer.idOfItem, function (err, res) {
                 if (err) failure(err);
                 success(res);
             });
         }).then(function (result) {
+            // quantity entered must be lower or equal to inventory 
             if (answer.quantityOfItem <= result[0].stock_quantity) {
+                // calculate new inventory to send to server 
                 var newQuantity = result[0].stock_quantity - answer.quantityOfItem;
+                // to display a dollar amount 
                 var totalPrice = parseFloat(result[0].price * answer.quantityOfItem).toFixed(2);
+                // set new amoutnt as the stock_quantity for selected item
                 connection.query("UPDATE products SET ? WHERE ?", [{
                     stock_quantity: newQuantity
                 }, {
                     item_id: result[0].item_id
                 }], function (err, res) {
                     console.log('Your purchase for $' + totalPrice + ' is complete!');
+                    // reset main menu 
                     reset();
                 });
             } else {
+                // if quantity entered exceeds quantity in inventory 
                 console.log('Insufficient Quantity!');
+                // reset main menu 
                 reset();
             }
         });
     });
 }
 
+// Destroys connection to server for Exit App
 function exitApp() {
     connection.destroy();
     console.log('\nFine! We hope you never come back!');
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,12 @@
+// request inquirer 
 var inquirer = require('inquirer');
+// request mysql package 
 var mysql = require('mysql');
+// link to key.js file for password of mysql
 var key = require('./key.js');
+// empty array set up for ID validation 
 var idArray = [];
+// connection settings
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -9,6 +14,7 @@ var connection = mysql.createConnection({
     password: key.mysqlKey.password,
     database: 'Bamazon'
 });
+// establish connection to server   
 connection.connect(function (err) {
     if (err) throw err;
     reset().catch(function (err) {
@@ -16,6 +22,7 @@ connection.connect(function (err) {
     });
 });
 
+// function that starts and reset the app (main menu)
 function reset() {
     return inquirer.prompt([{
         type: 'list',
@@ -23,30 +30,37 @@ function reset() {
         choices: ['View Product for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit App'],
         name: 'menu'
     }]).then(function (answer) {
+        // runs function to view products THEN reset main menu runs 
         if (answer.menu === 'View Product for Sale') {
             return viewProducts().then(function () {
                 reset();
             });
+        // runs function to view the low inventory
         } else if (answer.menu === 'View Low Inventory') {
             return lowInventory();
+        // runs function to view Product THEN add to inventory function runs 
         } else if (answer.menu === 'Add to Inventory') {
             return viewProducts().then(function () {
                 return addInventory();
             });
+        // runs new product function 
         } else if (answer.menu === 'Add New Product') {
             return addNewProduct();
+        // runs function to Exit app 
         } else if (answer.menu === 'Exit App') {
             return exitApp();
         }
     });
 }
 
+// function to add new products
 function addNewProduct() {
     return inquirer.prompt([{
         name: 'productName',
         type: 'input',
         message: 'Enter Name of New Product',
         validate: function (input) {
+            // validate that user entered 'something' at least
             if (input === '') {
                 console.log('Error: you must type something!');
                 return false;
@@ -59,6 +73,7 @@ function addNewProduct() {
         type: 'input',
         message: 'Enter Name of Department',
         validate: function (input) {
+            // input must be not nothing 
             if (input === '') {
                 console.log('\nError: you must type something!');
                 return false;
@@ -71,6 +86,7 @@ function addNewProduct() {
         type: 'input',
         message: 'Enter Price for New Product',
         validate: function (input) {
+            // input from user must be a number
             if (isNaN(input) !== true) {
                 return true;
             } else {
@@ -83,6 +99,7 @@ function addNewProduct() {
         type: 'input',
         message: 'Enter quantity to add to inventory',
         validate: function (input) {
+            // input from user must be a number
             if (isNaN(input) !== true) {
                 return true;
             } else {
@@ -91,6 +108,7 @@ function addNewProduct() {
             }
         }
     }]).then(function (answer) {
+        // add all inputs form user into server db
         return new Promise(function (success, failure) {
             var newName = answer.productName;
             var newDepartment = answer.productDepartment;
@@ -103,18 +121,21 @@ function addNewProduct() {
                 stock_quantity: newStock
             }], function (err, res) {
                 console.log('Added to products: ' + '"' + answer.productName + '"');
+                // restet the main menu after adding product... 
                 return reset();
             });
         });
     });
 }
 
+// function for adding inventory 
 function addInventory() {
     return inquirer.prompt([{
         name: 'idOfItem',
         type: 'input',
         message: 'Enter ID of item to add inventory to it',
         validate: function (input) {
+            //validation: must be a number, an ID smaller than length of idArray and can't be empty string 
             if ((isNaN(input) !== true) && (input <= idArray.length) && (input !== '')) {
                 return true;
             } else {
@@ -127,6 +148,7 @@ function addInventory() {
         type: 'input',
         message: 'Enter quantity to add to inventory',
         validate: function (input) {
+            // Cannot not be a number
             if (isNaN(input) !== true) {
                 return true;
             } else {
@@ -135,19 +157,23 @@ function addInventory() {
             }
         }
     }]).then(function (answer) {
+        // new Promise to select to THEN select all the ids of table 
         return new Promise(function (success, failure) {
             connection.query("SELECT * FROM products WHERE item_id=?", answer.idOfItem, function (err, res) {
                 if (err) failure(err);
                 success(res);
             });
         }).then(function (result) {
+            // calculate new inventory to send to server 
             var newQuantity = parseInt(result[0].stock_quantity) + parseInt(answer.quantityOfItem);
             connection.query("UPDATE products SET ? WHERE ?", [{
+                // set new amoutnt as the stock_quantity for selected item
                 stock_quantity: newQuantity
             }, {
                 item_id: result[0].item_id
             }], function (err, res) {
                 console.log('Inventory for ' + '"' + result[0].product_name + '"' + ' has been increased to ' + newQuantity);
+                // reset main menu
                 reset();
             });
         }).catch(function (err) {
@@ -156,11 +182,13 @@ function addInventory() {
     });
 }
 
+// display items function 
 function viewProducts() {
     return new Promise(function (success, failure) {
         connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw (err);
             success(res);
+            // displays every detail for each item 
             for (var i = 0; i < res.length; i++) {
                 console.log('ID#: ' + res[i].item_id + " | " + res[i].product_name + " | " + 'Dept: ' + res[i].department_name + " | " + '$' + res[i].price + " | " + "Qty: " + res[i].stock_quantity);
                 idArray.push(res[i].item_id);
@@ -169,6 +197,7 @@ function viewProducts() {
     });
 }
 
+// display function for items that have inventory lower than 5 
 function lowInventory() {
     return new Promise(function (success, failure) {
         connection.query("SELECT * FROM products", function (err, res) {
@@ -184,6 +213,7 @@ function lowInventory() {
     });
 }
 
+// Destroys connection to server for Exit App
 function exitApp() {
     connection.destroy();
     console.log('\nFine! We hope you never come back!');
